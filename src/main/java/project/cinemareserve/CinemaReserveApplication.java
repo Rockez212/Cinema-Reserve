41196b45e63801cecfeae33e6afa4d7d5fab6724
Lilian <ghertalilian2006@gmail.com>
2025-06-10 20:24:55 +0300

    init

---
+++ src/main/java/project/cinemareserve/CinemaReserveApplication.java
@@ -0,0 +1,13 @@
+package project.cinemareserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaReserveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaReserveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/cinemareserve/CinemaReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package project.cinemareserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaReserveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
