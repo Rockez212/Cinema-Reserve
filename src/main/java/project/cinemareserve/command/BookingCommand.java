150254bf1ccaf96c7767942ce659992b73e5e584
Lilian <ghertalilian2006@gmail.com>
2025-06-20 00:55:02 +0300

    -

--- src/main/java/project/cinemareserve/command/BookingCommand.java
+++ src/main/java/project/cinemareserve/command/BookingCommand.java
@@ -1,6 +1,7 @@
 package project.cinemareserve.command;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,8 +10,8 @@ import lombok.NoArgsConstructor;
 public class BookingCommand {
     @Min(value = 1, message = "movieId must be at least 1")
     private Long movieId;
-    @Min(value = 1, message = "row must be at least 1")
+    @Size(min = 1, max = 25, message = "The row must contain from 1 to 4 characters.")
     private int row;
-    @Min(value = 1, message = "number seat must be at least 1")
+    @Size(min = 1, max = 25, message = "The seat must contain from 1 to 25 characters.")
     private int numberSeats;
 }
